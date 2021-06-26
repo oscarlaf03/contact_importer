7d3dce740d283277511a46bf6a1aebeffab604f0
Oscar Ortiz <ortizg.oscar@gmail.com>
2021-06-26 12:49:56 -0300

    contact phone validator ok

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,22 @@
 class Contact < ApplicationRecord
   belongs_to :user
+  validates :email, presence: true, email: true
+  validates :address , presence: true
+  validates :phone, presence: true
+  before_save :ignore_phone, if: :invalid_phone_format?
+
+  private
+  def invalid_phone_format?
+    return true if not [22,19].include?(self.phone.length)
+    return true if /^\(\+\d{2}\)\s{1}/.match(self.phone).nil?
+    digits = self.phone[6, self.phone.length]
+    format_1_valid = /^\d{3}\s{1}\d{3}\s{1}\d{2}\s{1}\d{2}\s{1}\d{2}$/.match(digits).present?
+    format_2_valid = /^\d{3}\-{1}\d{3}\-{1}\d{2}\-{1}\d{2}$/.match(digits).present?
+    not ( format_1_valid or format_2_valid)
+  end
+
+  def ignore_phone
+    self.phone = nil
+  end
+
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -2,9 +2,85 @@ require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
   context "Validating attributes" do
-    context "Shuld not be valid" do
+    context "Should not be valid" do
       it "with invalid email address" do
-        
+        contact = build(:contact, email: 'invalid___email')
+        contact.valid?
+        assert contact.errors.details.keys.include?(:email)
+      end
+
+      it "with a nil email address" do
+        contact = build(:contact, email: nil)
+        contact.valid?
+        assert contact.errors.details.keys.include?(:email)
+      end
+      it "with a nil address" do
+        contact = build(:contact, address: nil)
+        contact.valid?
+        assert contact.errors.details.keys.include?(:address)
+      end
+      it "with a blank address" do
+        contact = build(:contact, address: "")
+        contact.valid?
+        assert contact.errors.details.keys.include?(:address)
+      end
+      it "with a nil phone" do
+        contact = build(:contact, phone: nil)
+        contact.valid?
+        assert contact.errors.details.keys.include?(:phone)
+      end
+      it "with a blank phone" do
+        contact = build(:contact, phone: "")
+        contact.valid?
+        assert contact.errors.details.keys.include?(:phone)
+      end
+    end
+
+    context "Should ignore invalid phone formats" do
+      it "Ignores invalid phone format  \"123 123\"" do
+        contact = create(:contact, phone:'123 123')
+        assert contact.persisted?
+        assert contact.phone.nil?
+      end
+
+      it "Ignores invalid phone format \"(+00) 000 000 00 00 00 000\" " do
+        contact = create(:contact, phone:'(+00) 000 000 00 00 00 000')
+        assert contact.persisted?
+        assert contact.phone.nil?
+      end
+
+      it "Ignores invalid phone format \"(+00) 000-000-00\" " do
+        contact = create(:contact, phone:'(+00) 0000-000-00')
+        assert contact.persisted?
+        assert contact.phone.nil?
+      end
+
+      it "Ignores invalid phone format \"(++42) 123-321-21-12\" " do
+        contact = create(:contact, phone:'(++42) 123-321-21-12')
+        assert contact.persisted?
+        assert contact.phone.nil?
+      end
+
+      it "Ignores invalid phone format \"(-42) 123-321-21-12\" " do
+        contact = create(:contact, phone:'(-42) 123-321-21-12')
+        assert contact.persisted?
+        assert contact.phone.nil?
+      end
+
+    end
+
+    context "Should persist valid phone phone formats"do
+
+      it "Persists valid phone format \"(+55) 119 236 42 16 75\"" do
+        contact = create(:contact, phone:'(+55) 119 236 42 16 75')
+        assert contact.persisted?
+        assert contact.phone.present?
+      end
+
+      it "Persists valid phone format \"(+42) 123-321-21-12\"" do
+        contact = create(:contact, phone:'(+42) 123-321-21-12')
+        assert contact.persisted?
+        assert contact.phone.present?
       end
 
     end
